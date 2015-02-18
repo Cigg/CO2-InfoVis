b0e5c74626ff811d8ff571d0924e7f11f00d0fc6
Oscar Westberg <oscar.henrik.westberg@gmail.com>
2015-02-18 13:54:44 +0100

    Added data categorization
    Data from Global Energy Use is now neatly stored in a variable

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
+var energyData = {};
+
 //Load data
 d3.csv("data/globalenergyuse-production.csv", function(error, data) {
-  console.log(JSON.stringify(data));
+	// Loop through data and put in correct category in energyData
+	for (var i = 0; i < data.length; i++) {
+		// If the current category dosen't exist yet, create new array
+		if(!energyData[data[i]["Series Name"]])
+			energyData[data[i]["Series Name"]] = new Array();
+
+		// Push data to correct property array
+		energyData[data[i]["Series Name"]].push(data[i]);
+	};
+
+	// That's beautiful!
+	console.log(energyData);
 });
