559ba3172064b72f414a0c8d07fd77bf4d8de9e0
Mikael Zackrisson <micke.zackrisson@gmail.com>
2015-02-18 12:20:24 +0100

    boilerplate

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var sp1 = new sp();
+var pc1 = new pc();
+var map = new map();
+
+//initialize color scale
+var colors = {
+  "Australia": "#a6cee3",
+  "Austria": "#1f78b4",
+  "Belgium": "#b2df8a",
+  "Canada": "#33a02c",
+  "Chile": "#fb9a99",
+  "Czech Republic": "#e31a1c",
+  "Denmark": "#fdbf6f",
+  "Estonia": "#ff7f00",
+  "Finland": "#cab2d6",
+  "France": "#fbb4ae",
+  "Germany": "#b3cde3",
+  "Greece": "#ccebc5",
+  "Hungary": "#decbe4",
+  "Iceland": "#fed9a6",
+  "Ireland": "#ffffcc",
+  "Israel": "#e5d8bd",
+  "Italy": "#fddaec",
+  "Japan": "#f2f2f2",
+  "Korea": "#e41a1c",
+  "Luxembourg": "#377eb8",
+  "Mexico": "#4daf4a",
+  "Netherlands": "#984ea3",
+  "New Zealand": "#ff7f00",
+  "Norway": "#ffff33",
+  "Poland": "#a65628",
+  "Portugal": "#f781bf",
+  "Slovak Republic": "#999999",
+  "Slovenia": "#8dd3c7",
+  "Spain": "#ffffb3",
+  "Sweden": "#bebada",
+  "Switzerland": "#fb8072",
+  "Turkey": "#80b1d3",
+  "United Kingdom": "#fdb462",
+  "United States": "#b3de69",
+  "Brazil": "#fccde5",
+  "Russian Federation": "#d9d9d9"
+};
