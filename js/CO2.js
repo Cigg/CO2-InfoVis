c846de47756cb3aa6dda07dd1df73f885a723ef0
Oscar Westberg <oscar.henrik.westberg@gmail.com>
2015-02-19 15:31:37 +0100

    Start of co2 and map

--- js/CO2.js
+++ js/CO2.js
@@ -1,3 +1,60 @@
 // -----------------------------------------
 // Displays a graph of CO2 emission over time
 // -----------------------------------------
+
+// Grab the CO2 div with jquery
+var CO2Div = $("#co2");
+
+// width and height is based on container div size
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = CO2Div.width() - margin.right - margin.left,
+    height = CO2Div.height() - margin.top - margin.bottom;
+
+// Create the svg area to draw in
+var svg = d3.select("#co2").append("svg")
+	.attr("width", width)
+	.attr("height", height)
+	.append("g");
+
+// Setup scales and axes
+var x = d3.scale.linear()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+draw();
+
+function draw()
+{
+    // Add x axis and title.
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .text("X axis")
+        .attr("class", "label")
+        .attr("x", width/2)
+        .attr("y", 30);
+        
+    // Add y axis and title.
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .text("Y axis")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -35)
+        .attr("x", -height/2)
+        .attr("dy", ".71em");
+}
--- js/map.js
+++ js/map.js
@@ -1,3 +1,43 @@
 // -----------------------------------------
 // Displays a Chroropleth map of all countries
 // -----------------------------------------
+
+// Grab the map div with jquery
+var mapDiv = $("#map");
+
+// width and height is based on container div size
+var margin = {top: 20, right: 20, bottom: 20, left: 20},
+    width = mapDiv.width() - margin.right - margin.left,
+    height = mapDiv.height() - margin.top - margin.bottom;
+
+// Create the svg area to draw in
+var svg = d3.select("#map").append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+// The view in the map
+var projection = d3.geo.mercator()
+    .center([90, 20 ])
+    .scale(150);
+
+// Draws edges in the map
+var path = d3.geo.path().projection(projection);
+// Create graphics variable
+g = svg.append("g");
+
+// load data and draw the map
+d3.json("data/world-topo.json", function(error, world) {
+    var countries = topojson.feature(world, world.objects.countries).features;
+    draw(countries);
+});
+
+function draw(countries,data) {
+    var country = g.selectAll(".country").data(countries);
+
+    country.enter().insert("path")
+        .attr("class", "country")
+        .attr("d", path)
+        .style("fill", function(d) {
+			return "#F52525"
+        })
+};
